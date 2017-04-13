8ec0ba429d67f7269920d8180131ea71293000aa
Pierre Brossy <pierre.brossy@ensimag.grenoble-inp.fr>
2017-04-13 09:59:00 +0200

    ajout de init _freq

--- src/BST.c
+++ src/BST.c
@@ -1,22 +1,14 @@
 #include "BST.h"
 
-
-int **tab_create(const int colonne, const int ligne, int size) 
+void init_tabf(double **tab2f, double *tab_lu, long n) 
 {
-	int** tab = malloc(size*colonne);
-	for (long i = 0; i < colonne; ++i)
-	{
-		(*tab)[i] = malloc(size*ligne);
-	}
-	return tab;
-}
-
-void destruct_tab(int*** tab, int colonne) 
-{
-	for (long i = 0; i < colonne; ++i)
-	{
-		free((*tab)[i]);
+	for (int i = 0; i < n ; ++i) {
+		for (int j = i; j < n ; ++j) {
+			if (j == 0) {
+				tab2f[i][j] = tab_lu[j];
+			} else {
+				tab2f[i][j] = tab2f[i][j-1] +tab_lu[j];
+			}
+		}
 	} 
-	free(tab);
 }
-
